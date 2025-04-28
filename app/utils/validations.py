5892ddc40fab842c09e53d35f15496d67d2f0e5e
Daniel Valenzuela Martínez <daniel.valenzuela.m@ug.uchile.cl>
2025-04-28 18:39:51 -0400

    Rename validation functions to Spanish and add phone number validation

--- app/utils/validations.py
+++ app/utils/validations.py
@@ -1,9 +1,11 @@
 import re
 import filetype
 
-def validate_name (value):
+def validar_nombre (value):
     return value and len(value) > 3
 
-def validate_email (value):
+def validar_email (value):
     return "@" in value
 
+def validar_telefono (value):
+    return re.match(r'^\d{10}$', value) is not None
