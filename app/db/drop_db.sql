a70b7d8516d1714de0d938b6332166d1a12cff56
Daniel Valenzuela Martínez <daniel.valenzuela.m@ug.uchile.cl>
2025-05-02 19:19:40 -0400

    Add SQL scripts for database drop and user creation

---
+++ app/db/drop_db.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS `tarea2`;
+
+DROP TABLE IF EXISTS `tarea2`.`actividad`;
+
+DROP TABLE IF EXISTS `tarea2`.`comuna`;
+
+DROP TABLE IF EXISTS `tarea2`.`region`;
+
+DROP TABLE IF EXISTS `tarea2`.`actividad_tema`;
+
+DROP TABLE IF EXISTS `tarea2`.`contactar_por`;
+
+DROP TABLE IF EXISTS `tarea2`.`foto`;
---
+++ app/db/user.sql
@@ -0,0 +1,7 @@
+-- Active: root:fox17@localhost/tarea2
+
+-- Crear usuario
+CREATE USER 'dbadmin'@'localhost' IDENTIFIED BY 'dbadmin';
+
+-- Eliminar usuario de ser necesario
+DROP USER 'dbadmin'@'localhost';
