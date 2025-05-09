09302f64d670cbb420b0f01c085a36a586555d45
Daniel Valenzuela Martínez <daniel.valenzuela.m@ug.uchile.cl>
2025-05-08 22:16:16 -0400

    Reorder comments and SQL statements in user.sql for better clarity

--- app/db/user.sql
+++ app/db/user.sql
@@ -3,11 +3,11 @@
 -- Crear usuario
 CREATE USER 'cc5002'@'localhost' IDENTIFIED BY 'programacionweb';
 
--- Eliminar usuario de ser necesario
-DROP USER 'cc5002'@'localhost';
-
 -- Otorgar todos los privilegios al usuario cc5002 en la base de datos tarea2
 GRANT ALL PRIVILEGES ON tarea2.* TO 'cc5002'@'localhost';
 
 -- Actualizar los privilegios
 FLUSH PRIVILEGES;
+
+-- Eliminar usuario de ser necesario
+DROP USER 'cc5002'@'localhost';
