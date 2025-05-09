bd9d6646e6a8c60f8148d2bb102db0686e4b234d
Daniel Valenzuela Martínez <daniel.valenzuela.m@ug.uchile.cl>
2025-05-08 22:01:55 -0400

    Add privileges for user cc5002 in user.sql and flush privileges

--- app/db/user.sql
+++ app/db/user.sql
@@ -5,3 +5,9 @@ CREATE USER 'cc5002'@'localhost' IDENTIFIED BY 'programacionweb';
 
 -- Eliminar usuario de ser necesario
 DROP USER 'cc5002'@'localhost';
+
+-- Otorgar todos los privilegios al usuario cc5002 en la base de datos tarea2
+GRANT ALL PRIVILEGES ON tarea2.* TO 'cc5002'@'localhost';
+
+-- No olvides actualizar los privilegios
+FLUSH PRIVILEGES;
