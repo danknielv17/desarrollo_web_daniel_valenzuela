bdf5784d0af0fd703c769f36654b0d1c5b66ea27
Daniel Valenzuela Martínez <daniel.valenzuela.m@ug.uchile.cl>
2025-05-12 19:43:30 -0400

    Enhance email validation regex in validations.py

--- app/utils/validations.py
+++ app/utils/validations.py
@@ -4,8 +4,9 @@ import filetype
 def validar_nombre (value):
     return value and len(value) > 3
 
-def validar_email (value):
-    return "@" in value
+def validar_email(value):
+    pattern = r'^[\w.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,6}$'
+    return re.match(pattern, value) is not None
 
 def validar_telefono (value):
     return re.match(r'^\d{10}$', value) is not None
